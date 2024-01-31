870570b23e09e9111d1edb7abd3bf4b79a94d244
rauanaakhmetova <157012475+rauanaakhmetova@users.noreply.github.com>
2024-02-01 00:31:35 +0600

    Lab1

---
+++ Variables.py
@@ -0,0 +1,39 @@
+#ex1
+carname = "Volvo"
+#ex2
+x = 50
+#ex3
+x = 5
+y = 10
+print(x + y)
+#ex4
+x = 5
+y = 10
+z = x + y
+print(z)
+#ex5
+x, y, z = "Orange", "Banana", "Cherry"
+#ex6
+x = y = z = "Orange"
+#ex7
+def myfunc():
+  global x
+  x = "fantastic"
+
+
+#Examples
+#1Create a Variable
+x = 5
+y = "John"
+print(x)
+print(y)
+#2Output both text and a variable
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+#3Add a variable to another variable
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
